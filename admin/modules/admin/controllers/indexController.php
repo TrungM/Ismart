776333a6df3092c64fd70bac975f416e71262196
TrungM <you@example.com>
2023-01-03 20:25:59 +0700

    Ismart

--- admin/modules/admin/controllers/indexController.php
+++ admin/modules/admin/controllers/indexController.php
@@ -61,7 +61,7 @@ function indexAction(){
                                                   $error["email"]="Không đúng định dạng";
 
                                         }else{
-                                                  $email=$_POST["email"];
+                                                  $email=$_POST["emaile"];
 
                                         }
                               }
